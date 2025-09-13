28e65ed73417d5c49d7e070da40b070bc401ab40
younghunkimm <hunifive@gmail.com>
2025-09-13 23:22:25 +0900

    feat(todo): `Todo` 조건 검색 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,5 +1,7 @@
 package org.example.expert.domain.todo.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,15 +9,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
+    @Query("""
+            SELECT t
+            FROM Todo t
+            LEFT JOIN FETCH t.user u
+            WHERE (COALESCE(:weather, '') = '' OR t.weather LIKE CONCAT('%', :weather, '%'))
+                AND (COALESCE(:startDate, '') = '' OR FUNCTION('DATE', t.modifiedAt) >= :startDate)
+                AND (COALESCE(:endDate, '') = '' OR FUNCTION('DATE', t.modifiedAt) <= :endDate)
+            ORDER BY t.modifiedAt DESC
+        """)
+    Page<Todo> searchWithUser(
+        @Param("weather") String weather,
+        @Param("startDate") LocalDate startDate,
+        @Param("endDate") LocalDate endDate,
+        Pageable pageable
+    );
+
     @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
+        "LEFT JOIN t.user " +
+        "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
