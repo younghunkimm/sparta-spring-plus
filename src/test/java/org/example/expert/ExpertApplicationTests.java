f698f4eb1ee5874d661ea8ae6af8d68d998fb577
younghunkimm <hunifive@gmail.com>
2025-09-17 02:17:26 +0900

    fix(build): 빌드 실패 해결을 위해 `wrapper tasks` 실행 및 test용 설정파일 추가

--- src/test/java/org/example/expert/ExpertApplicationTests.java
+++ src/test/java/org/example/expert/ExpertApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.expert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpertApplicationTests {
 
     @Test
