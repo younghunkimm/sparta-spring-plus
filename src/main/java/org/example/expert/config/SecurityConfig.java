31b77c23d13898ef924755e433811b38d09f69d2
younghunkimm <hunifive@gmail.com>
2025-09-17 13:07:42 +0900

    feat(actuator): `Spring Actuator` 추가

--- src/main/java/org/example/expert/config/SecurityConfig.java
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
 
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
+                .requestMatchers(("/actuator/**")).permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, SecurityContextHolderAwareRequestFilter.class)
