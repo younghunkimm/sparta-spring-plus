04879f56d5a8210aadfacbfb2252f7650a38bd78
younghunkimm <hunifive@gmail.com>
2025-09-22 00:15:04 +0900

    fix(user, bulk): Bulk Insert 최소, 최대 조건 추가

--- src/main/java/org/example/expert/domain/user/dto/request/UserBulkInsertRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserBulkInsertRequest.java
@@ -1,9 +1,13 @@
 package org.example.expert.domain.user.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 
 @Getter
 public class UserBulkInsertRequest {
 
+    @Min(1)
+    @Max(5_000_000)
     private int count;
 }
