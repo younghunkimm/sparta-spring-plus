0fd435a3c160145f1e14098bb59ff74de0f55d94
younghunkimm <hunifive@gmail.com>
2025-09-22 14:11:41 +0900

    feat(user): 닉네임 일치 유저 목록 조회 API 구현

--- src/main/java/org/example/expert/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/expert/domain/user/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package org.example.expert.domain.user.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.example.expert.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
+
+    List<User> findByNickname(String nickname);
 }
